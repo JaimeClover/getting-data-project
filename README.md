d1bbb067b531eafa2457996d3144a8a8d5941798
Brett Pilcher <brettpilch@yahoo.com>
2015-02-17 23:26:41 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+This repository contains the files for my course project for the Coursera class "Getting and Cleaning Data". There are two files in addition to this README:
+
+run_analysis.R
+CodeBook.md
+
+run_analysis.R is an R script that will output the file "mean_values.txt", which shows the average value of certain variables for each activity/subject combination from the original data sets.
+
+CodeBook.md contains a list of all the variable names used in "mean_values.txt", and it explains the meaning of each variable name.
+
+Instructions for running this code:
+* If you don't already have the data files downloaded for this project, download them here: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip and unzip the folder.
+* Open R and set the working directory to the folder containing the data files.
+* Run the script "run_analysis.R" (this will take a few minutes on slower computers)
+* This will create a file "mean_values.txt", which should be the same as what I submitted in part 1 of the assignment.
---
+++ run_analysis.R
@@ -0,0 +1,53 @@
+# With "UCI HAR Dataset 2" as your working directory, this script does the following: 
+# 1. Merges the training and the test sets to create one data set.
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+# 3. Uses descriptive activity names to name the activities in the data set
+# 4. Appropriately labels the data set with descriptive variable names. 
+# 5. From the data set in step 4, creates a second, independent tidy data set 'mean_values.txt'
+#    with the average of each variable for each activity and each subject.
+
+# load training and test sets, and combine into one data frame:
+train1 <- read.table("train/X_train.txt")
+test1 <- read.table("test/X_test.txt")
+data <- rbind(train1, test1)
+
+# Using features.txt, select columns with "mean" or "std" in the name and rename them:
+features <- read.table("features.txt")
+index <- grep("mean|std", features$V2)
+dataMeanStd <- data[, index]
+names(dataMeanStd) <- features[index, "V2"]
+
+# load Y_train.txt and Y_test.txt, and combine them into one data frame:
+trainY <- read.table("train/Y_train.txt")
+testY <- read.table("test/Y_test.txt")
+combinedY <- rbind(trainY, testY)
+
+# Using activity_labels.txt, add a column of activity labels to the main data frame:
+activityLabels <- read.table("activity_labels.txt")$V2
+labeledY <- activityLabels[combinedY$V1]
+dataMeanStd$activity = labeledY
+
+# load subject data, combine them, and add a column to the main data frame:
+trainSubject <- read.table("train/subject_train.txt")
+testSubject <- read.table("test/subject_test.txt")
+combinedSubject <- rbind(trainSubject, testSubject)
+dataMeanStd$subject = combinedSubject$V1
+
+# split the data into a different group for each activity/subject combination:
+splitter <- split(dataMeanStd, list(dataMeanStd$activity, dataMeanStd$subject))
+
+# create new data frame with the column means of each variable in the first activity/subject group:
+output <- data.frame(t(colMeans(splitter[[1]][,1:79])))
+names(output) <- paste("Mean.Of", names(output), sep=".")
+output <- cbind(splitter[[1]][1, 80:81], output) # add columns for activity and subject
+
+# loop through the rest of the groups, and append their column means to the data frame:
+for (i in 2:180) {
+    splitMeans <- data.frame(t(colMeans(splitter[[i]][,1:79])))
+    names(splitMeans) <- paste("Mean.Of", names(splitMeans), sep=".")
+    splitMeans <- cbind(splitter[[i]][1, 80:81], splitMeans)
+    output <- rbind(output, splitMeans)
+}
+
+# create a new file for this data set:
+write.table(output, "mean_values.txt", row.names = FALSE)
